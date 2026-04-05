bdd0fdbcdbc2a0cacc2d9bd047c86b00d1e544f4
Lidiane Mendes <lidyaraujo64@gmail.com>
2026-04-05 14:07:15 -0300

    lidiane

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home(){
-    return <h1>Margarida Mendes Araujo</h1>;
+export default function Home() {
+  return (
+    <>
+      <h1>Lidiane Mendes</h1>
+      <p>Lidiane</p>
+    </>
+  );
 }
-
-export default Home;

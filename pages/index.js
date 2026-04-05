50236104abc25964659a849dbbdda98b378e0a7b
Lidiane Mendes <lidyaraujo64@gmail.com>
2026-04-05 14:18:14 -0300

    Inserindo CSS

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
   return (
-    <>
+    <main className={styles.container}>
+
+      <img 
+        src="/sua-foto.jpg" 
+        alt="Foto de Lidiane"
+        className={styles.foto}
+      />
+
       <h1>Lidiane Mendes</h1>
-      <p>Lidiane</p>
-    </>
+
+      <p className={styles.descricao}>
+        Desenvolvedora Java em formação
+      </p>
+
+      <div className={styles.links}>
+        <a href="#">GitHub</a>
+        <a href="#">LinkedIn</a>
+      </div>
+
+    </main>
   );
 }

8c456ec15fdc2622e97852426a8fc909195090d6
Lidiane Mendes <103313268+LidianeMendess@users.noreply.github.com>
2026-04-06 06:37:24 -0300

    Atualizar o index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </p>
 
       <div className={styles.links}>
-        <a.  href="https://www.linkedin.com/in/lidiane-mendess" target="_blank">
+        <a href="https://www.linkedin.com/in/lidiane-mendess" target="_blank">
   LinkedIn
        </a>
       </div>
